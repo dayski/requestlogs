1c666f4a8e984e25adc5a242a2c436664f063d75
Kapil Bharati <kapil.bharati@delhivery.com>
2013-08-27 15:58:18 +0530

    clean up README files

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from distutils.core import setup
 
 setup(
     name='RequestLogs',
-    version='0.2adev',
+    version='0.2.1dev',
     author='dayski',
     author_email='kapil@delhivery.com',
     packages=['requestlogs',],
     url='https://github.com/dayski/requestlogs',
     license='LICENSE.txt',
     description='Request Log middleware',
-    long_description=open('README').read(),
+    long_description=open('README.rst').read(),
     install_requires=[
         "Django >= 1.3.1",
         "pymongo >= 2.5.1",
