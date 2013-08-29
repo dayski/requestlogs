32c863c5dd3f965894f6ca3525e0f46409af96ff
Kapil Bharati <kapil.bharati@delhivery.com>
2013-08-30 00:18:16 +0530

    fixed README.rst error in setup

--- setup.py
+++ setup.py
@@ -1,15 +1,20 @@
 from distutils.core import setup
 
+
+def readme():
+        with open('README.rst') as f:
+                    return f.read()
+
 setup(
     name='RequestLogs',
-    version='0.2.1dev',
+    version='0.3dev',
     author='dayski',
     author_email='kapil@delhivery.com',
     packages=['requestlogs',],
     url='https://github.com/dayski/requestlogs',
-    license='LICENSE.txt',
+    license='MIT',
     description='Request Log middleware',
-    long_description=open('README.rst').read(),
+    long_description=readme(),
     install_requires=[
         "Django >= 1.3.1",
         "pymongo >= 2.5.1",
